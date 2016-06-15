ea4e5b342e18a59d40b6de3866e0dcae4694c1c2
LexKenji <alexis.kenji@gmail.com>
2016-06-15 09:09:43 -0300

    Update_v14

---
+++ src/Business/PacoteDAO.java
@@ -0,0 +1,103 @@
+package Business;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import Product.Produto;
+import User.MyConnection;
+
+import java.time.LocalDate;
+import java.sql.SQLException;
+
+public class PacoteDAO {
+	
+	private static PacoteDAO instance = new PacoteDAO();
+	
+	private String sql;
+	
+	private PacoteDAO(){}
+	
+	public static PacoteDAO getInstance(){
+		return instance;
+	}
+	
+	public int insertPacote(String periodicidade, LocalDate createDate, Assinatura assinatura, boolean validity, int quantidade, Set<Produto> produtos) throws SQLException {
+        Connection conn = MyConnection.getConnection();
+        PreparedStatement stmnt = null;
+        try{
+            sql = "INSERT INTO cerveja.pacote (periodicidade, dataCriacao, assinaturaID, validade, quantidade) VALUES (?, ?, ?, ?, ?)";
+            stmnt = conn.prepareStatement(sql);
+            stmnt.setString(1, periodicidade);
+            stmnt.setDate(2, Date.valueOf(createDate));
+            stmnt.setInt(3, assinatura.getID());
+            stmnt.setBoolean(4, validity);
+            stmnt.setInt(5, quantidade);
+            stmnt.executeUpdate();
+        }
+        catch(Exception e){
+            throw new SQLException("Erro ao inserir pacote", e);
+        }
+        finally{
+            if(stmnt != null) stmnt.close();
+        }
+        Statement getPacoteID = null;
+        ResultSet rsPacoteID = null;
+        int pacoteID = 0;
+        try{
+            sql = "SELECT MAX(pacoteID) AS lastID FROM cerveja.pacote";
+            getPacoteID = conn.createStatement();
+            rsPacoteID = getPacoteID.executeQuery(sql);
+            while(rsPacoteID != null && rsPacoteID.next()){
+                pacoteID = rsPacoteID.getInt(1);
+            }
+        }
+        catch(Exception e){
+            throw new SQLException("Erro ao pegar ID do pacote", e);
+        }
+        finally{
+            if(getPacoteID != null) getPacoteID.close();
+            if(rsPacoteID != null) rsPacoteID.close();
+        }
+        
+        Produto[] produtosArr = produtos.toArray(new Produto[produtos.size()]);
+        for(int i = 0; i < produtos.size(); i++){
+            PreparedStatement stmnt2 = null;
+            try{
+                sql = "INSERT INTO cerveja.pacote_produto (pacoteID, produtoID) VALUES (?, ?)";
+                stmnt2 = conn.prepareStatement(sql);
+                stmnt2.setInt(1, pacoteID);
+                stmnt2.setInt(2, produtosArr[i].getID());
+                stmnt2.executeUpdate();
+            }
+            catch(Exception e){
+                throw new SQLException("Erro ao inserir pacote_produto " + i, e);
+            }
+            finally{
+                if(stmnt2 != null) stmnt2.close();
+            }
+        }
+        if(conn != null) conn.close();
+        return pacoteID;
+    }
+    
+    public void deletePacote(int pacoteID) throws SQLException {
+        Connection conn = MyConnection.getConnection();
+		PreparedStatement stmnt = null;
+		try{
+			sql = "DELETE FROM cerveja.pacote WHERE pacoteID = ?";
+			stmnt = conn.prepareStatement(sql);
+			stmnt.setInt(1, pacoteID);
+			stmnt.execute();
+		}
+		catch(Exception e){
+			throw new SQLException("Erro ao deletar pacote " + pacoteID, e);
+		}
+        finally {
+			if (stmnt != null) stmnt.close();
+			if (conn!= null) conn.close();
+		}
+    }
+
+}
---
+++ src/Business/PagamentoDAO.java
@@ -0,0 +1,44 @@
+package Business;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+import User.MyConnection;
+
+import java.sql.SQLException;
+
+public class PagamentoDAO {
+	
+	private static PagamentoDAO instance = new PagamentoDAO();
+	
+	private String sql;
+	
+	private PagamentoDAO(){}
+	
+	public static PagamentoDAO getInstance(){
+		return instance;
+	}
+	
+	
+    public void insertPagamento(int valor, LocalDate dataPagamento, Assinatura assinatura) throws SQLException {
+        Connection conn = MyConnection.getConnection();
+        PreparedStatement stmnt = null;
+        try{
+            
+            sql = "INSERT INTO cerveja.pagamento (valor, dataPagamento, assinaturaID) VALUES (?, ?, ?)";
+            stmnt = conn.prepareStatement(sql);
+            stmnt.setInt(1, valor);
+            stmnt.setDate(2, Date.valueOf(dataPagamento));
+            stmnt.setInt(3, assinatura.getID());
+            stmnt.executeUpdate();
+        }
+        catch(Exception e){
+            throw new SQLException("Erro ao inserir pagamento", e);
+        }
+        finally{
+            if(stmnt != null) stmnt.close();
+            if(conn != null) conn.close();
+        }
+    }
+
+}
